fc1d21e33bba8a92e59d43d157e86976f90ba9e8
hrsano645 <hrs.sano645@gmail.com>
2024-01-24 10:06:29 +0900

    update createfile task

--- app_file.py
+++ app_file.py
@@ -2,15 +2,12 @@ import os
 from time import sleep
 import redis
 from rq import Queue
-from tasks import add
+from tasks_file import run_task
 
 q = Queue(connection=redis.from_url(os.environ.get("RQ_REDIS_URL")))
 
 # 10個のタスクの実行をキューに投げる
-tasks = [q.enqueue(add, args=(i, 1)) for i in range(10)]
+tasks = [q.enqueue(run_task) for i in range(10)]
 
 # タスク実行が完了するまで少し待つ
 sleep(1)
-
-# 結果を出力する
-print([task.result for task in tasks])
---
+++ tasks_file.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+import random
+import string
+import sys
+
+NUM_FILES = 100
+FILE_SIZE = 1048576
+
+
+def create_random_string(length):
+    """指定された長さのランダムな文字列を生成する関数"""
+    letters = string.ascii_letters + string.digits
+    return "".join(random.choice(letters) for i in range(length))
+
+
+def create_files(num_files, file_size, directory="test_files"):
+    """指定された数とサイズのファイルを生成する関数"""
+    Path(directory).mkdir(parents=True, exist_ok=True)
+
+    for i in range(num_files):
+        filename = f"{directory}/file_{i}.txt"
+        with open(filename, "w") as f:
+            f.write(create_random_string(file_size))
+
+
+def run_task():
+    """タスクを実行する関数"""
+    create_files(NUM_FILES, FILE_SIZE)
+    return "Done!"
+
+
+# このファイルが直接実行された場合（テスト用）
+# win: Measure-Command{ python tasks_file.py 100 1048576}
+# mac: time python tasks_file.py 100 1048576
+if __name__ == "__main__":
+    num_files = int(sys.argv[1])
+    file_size = int(sys.argv[2])
+
+    create_files(num_files, file_size)
