70cf9b865199641999d54c284bc9008930cb7a13
hrsano645 <hrs.sano645@gmail.com>
2024-01-24 12:19:08 +0900

    update

--- file_task/app.py
+++ file_task/app.py
@@ -1,21 +1,17 @@
 import os
-from time import sleep
 import redis
 from rq import Queue
 from tasks import create_files
 
 
-NUM_FILES = 100
+NUM_FILES = 50
 FILE_SIZE = 1048576
 NUM_TASKS = 3
 
 q = Queue(connection=redis.from_url(os.environ.get("RQ_REDIS_URL")))
 
-# 10個のタスクの実行をキューに投げる
+# タスクの実行をキューに投げる
 tasks = [
     q.enqueue(create_files, args=(NUM_FILES, FILE_SIZE, f"test_files_{i}"))
     for i in range(NUM_TASKS)
 ]
-
-# タスク実行が完了するまで少し待つ
-sleep(1)
